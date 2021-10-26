14cccac6678b403526b19daf7ea4532d89915567
DESKTOP-EO39B50\LENOVO PC <niswaayu99@gmail.com>
2021-10-26 10:26:15 +0700

    Selamat Pagi

---
+++ Niswa-60900119022/resources/views/biodata.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1> Biodata </H1>
+    <H3> Nama : Niswa Ayu Lestari </H3>
+    <H3> Kelas: A </H3>
+    <H3> NIM  : 60900119022 </H3>
+    <H3> Email: 60900119022@uin-alauddin.ac.id </H3>
+    
+</body>
+</html>
